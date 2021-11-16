59ccfd645a004793e9782f0d94ca51c7fcf63d2a
jimlemoine <jlemoine007@gmail.com>
2021-11-15 16:18:22 -0800

    class get and post working

--- api/classes/classes-model.js
+++ api/classes/classes-model.js
@@ -1,10 +1,21 @@
 const db = require('../data/db-config');
 
 const getAllClasses = async () => {
-    const rows = await db('classes as c')
-        .leftJoin()
+    return db('classes')
+}
+
+const getClassById = async (id) => {
+    const result = await db('classes').where('class_id', id).first();
+    return result;
+}
+
+const insertClass = async (resource) => {
+    const [newClassObject] = await db('classes').insert(resource, ['class_id', 'name', 'instructor_username', 'type', 'start_time', 'duration', 'intensity', 'location', 'class_size'])
+  return newClassObject
 }
 
 module.exports = {
-    getAllClasses
+    getAllClasses,
+    getClassById,
+    insertClass
 };
--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const Classes = require('./classes-model');
 
-// router.get('/', (req, res) => {
-    
-// })
+router.get('/', async (req, res) => {
+    res.json(await Classes.getAllClasses())
+})
+
+router.post('/', async (req, res) => {
+    res.status(201).json(await Classes.insertClass(req.body))
+  })
 
 module.exports = router;
